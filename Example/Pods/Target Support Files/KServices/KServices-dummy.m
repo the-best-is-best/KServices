bbf50bcc14b9717ade7c4ce6ceca27402110651f
Michelle Raouf <michelle.raouf@52ndsolution.net>
2024-09-22 23:02:04 +0300

    support package manager

---
+++ Example/Pods/Target Support Files/KServices/KServices-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KServices : NSObject
+@end
+@implementation PodsDummy_KServices
+@end
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,5 +1,4 @@
 import XCTest
-import KServices
 
 class Tests: XCTestCase {
     
